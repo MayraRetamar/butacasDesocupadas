a2790de138897ffb97325b29a7ef7421257ea176
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-27 15:39:22 -0300

    cambio Agregue enunciado

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+/*Diseñar un algoritmo que recorra las 
+butacas de una sala de cine y determine 
+cuántas butacas desocupadas hay 
+• Suponga que para modelar este 
+problema, se utiliza un arreglo con 
+valores lógicos
+• La presencia de un valor verdadero (true) en 
+el arreglo indica que la butaca está ocupada
+• La presencia de un valor falso (false) en el 
+arreglo indica que la butaca está desocupada*/
+
 let cantidadButacas: number = Number(prompt("Ingrese la cantidad de butacas"));
 let butacas: boolean[] = new Array(cantidadButacas);
 
